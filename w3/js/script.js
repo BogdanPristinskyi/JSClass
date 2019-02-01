a4c8b68d8425db011599dc76f29bdb1c22f9fef6
BogdanPristinskyi <bog777@ukr.net>
2019-01-31 21:34:04 -0500

    End of class

--- w3/js/script.js
+++ w3/js/script.js
@@ -4,7 +4,8 @@
 
 
 // function isPalidromic(userInput){
-//   let userInputArray = userInput.split('');
+// let spaceFree - userInput.split(' ').join('');
+//   let userInputArray = userspaceFree.toLowerCase().split('');
 //
 //   let reversed = userInputArray.reverse();
 //
@@ -12,11 +13,21 @@
 //   return (userInput === reversedStr);
 //
 // }
-
+function submitForm(){
+  let text = document.getElementById('input').value;
+  alert(isPalidromic2(text));
+}
 
 function isPalidromic2(userInput){
   return (userInput
     .split('')
     .reverse()
-    .join('') === userInput);}
-console.log(isPalidromic2('mom'));
+    .join('')
+    .toLowerCase() === userInput.toLowerCase());}
+console.log(isPalidromic2('moM'));
+
+function generateRandom ( min = 0 , max = 1000){
+  return Math.floor(Math.random()* (max - min) + min) ;
+}
+let random = generateRandom ();
+console.log(random);
